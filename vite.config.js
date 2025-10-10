ea85272ce8f1194b73997c3f32a01601176e9347
Martin Droguett <martin.dtondro@gmail.com>
2025-10-10 15:20:19 -0300

    deploy: vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bienstar-total-public',
+  base: "./", // 👈 Esto genera rutas relativas compatibles con Vercel
   plugins: [react()],
-})
+});
