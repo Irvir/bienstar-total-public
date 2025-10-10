a76ac3ee83a0ae90a3b5119a7780407eeec61127
Martin Droguett <martin.dtondro@gmail.com>
2025-10-10 13:49:29 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles/Base.css';
-import './controllers/notify.js';
-import './controllers/alimentos.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/Base.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter basename="/bienstar-total-public">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
