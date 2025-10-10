7468c8f95e7cc7e68121b6206d84bad9a9d4161c
Martin Droguett <martin.dtondro@gmail.com>
2025-10-10 14:39:23 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/bienstar-total-public",
+  base: '/bienstar-total-public/',
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      cookie: '/src/empty-module.js'
+    }
+  }
+})
