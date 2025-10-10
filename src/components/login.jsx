4b36106ad39424904bea92a9fbf55f33ffaf22a5
Martin Droguett <martin.dtondro@gmail.com>
2025-10-10 14:26:53 -0300

    deploy

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Login.css";
 // Base.css is imported in main.jsx globally. Avoid duplicate imports here.
-import withAuth from "../components/withAuth";
+import withAuth from "./withAuth";
 
 
 export default function Login() {
