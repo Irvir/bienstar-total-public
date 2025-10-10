30603aa6787186a6845535cd2d5ad654b08ad7c0
Martin Droguett <martin.dtondro@gmail.com>
2025-10-09 21:37:28 -0300

    Configura basename para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/Base.css';
 import './controllers/notify.js';
@@ -7,6 +8,8 @@ import './controllers/alimentos.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/bienstar-total-public">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
