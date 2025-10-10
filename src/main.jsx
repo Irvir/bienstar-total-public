974be5dff0d74ad4bd4f27fd19af9fb94dfe5479
Martin Droguett <martin.dtondro@gmail.com>
2025-10-09 21:12:29 -0300

    Archivos a github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/Base.css';
-import './controllers/notify.js';
+import './controllers/Notify.js';
 import './controllers/Alimentos.js';
 
 createRoot(document.getElementById('root')).render(
