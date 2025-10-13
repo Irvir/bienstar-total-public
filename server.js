6b90eeeff8800a3d9048e8e683df6961bdb3d830
Martin Droguett <martin.dtondro@gmail.com>
2025-10-13 20:50:26 -0300

    fix: /* en el app.get

--- server.js
+++ server.js
@@ -572,7 +572,7 @@ app.post("/delete-diet-item", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
