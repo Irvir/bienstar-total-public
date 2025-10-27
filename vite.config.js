a0b474b138f4922a8d42ab0fedb3a9722523c758
Daniel Gutiérrez <fapenydanixx11@gmail.com>
2025-10-27 17:12:33 -0300

    arreglos v.2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[ext]'
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|avif/i.test(ext)) {
+            return `assets/images/[name][extname]`;
+          }
+          return `assets/[name][extname]`;
+        }
       }
     }
-  }
+  },
+  publicDir: 'public'
 })
